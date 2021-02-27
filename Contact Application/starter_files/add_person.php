0867d6e069a843cd19aa0272c73ee406215efce9
unknown <moumou.souhaila@gmail.com>
2021-02-27 20:39:07 +0100

    adding php files with css

---
+++ Contact Application/starter_files/add_person.php
@@ -0,0 +1,41 @@
+
+    <section id="add_person">
+        <div class="add">
+                <h1>ADD NEW PERSON</h1>
+        </div>
+
+        <div>
+            <form>
+                <div class="input-row">
+                    <div class="input-group">
+                        <label>First Name :</label>
+                        <input type="text">
+                    </div>
+                    <div class="input-group">
+                        <label>Last Name :</label>
+                        <input type="text">
+                    </div>
+                    <div class="input-group">
+                        <label>Email :</label>
+                        <input type="mail"require>
+                    </div>
+                    <div class="input-group">
+                        <label>Adress :</label>
+                        <input type="text">
+                    </div>
+                    <div class="input-group">
+                        <label>Phone :</label>
+                        <input type="number" require>
+
+                        <input type="checkbox"> <label for="checking">Friend</label>
+                        <input type="checkbox"> <label for="checking">Family</label>
+                        <input type="checkbox"> <label for="checking">Business</label>
+                    </div>
+
+                    <label>Notes :</label>
+                    <textarea rows="10"></textarea>
+
+                </div>
+            </form>
+            <button class="button"><span>SUBMIT </span></button>
+        </div>
---
+++ Contact Application/starter_files/login.php
@@ -0,0 +1,24 @@
+
+<div class="container">
+        <div class="main-box">
+            <div class="box-left">
+                <h1>WELCOME</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing Excepturi facere illum ullam exercitationem praesentium Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi facere illum ullam exercitationem praesentium Lorem, ipsum dolor sit amet consectetur adipisicing Excepturi facere illum ullam exercitationem praesentium Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi facere illum ullam exercitationem praesentium Lorem, ipsum dolor sit amet consectetur adipisicing Excepturi facere illum ullam.Lorem, ipsum dolor sit amet consectetur adipisicing Excepturi facere illum ullam exercitationem praesentium Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <button class="button"><span>Read More </span></button>
+            </div>
+
+            <div class="box-right">
+                <h1>LOGIN</h1>
+                <form>
+                    <div class="input-row">
+                        <label for="email">Email</label>
+                        <input type="email" id="email">
+                        <label for="pass">Password</label>
+                        <input type="password">
+                    </div>
+                        <input type="checkbox" value="lsRememberMe" id="rememberMe"> <label for="rememberMe">Remember me</label>
+                </form>
+                <button class="button"><span>Login </span></button>
+            </div>
+        </div>
+</div>
