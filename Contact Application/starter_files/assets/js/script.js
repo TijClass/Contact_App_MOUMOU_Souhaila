7eee9c24123b896a04baae3c9e7b0cf49b46e07b
unknown <moumou.souhaila@gmail.com>
2021-03-02 10:13:33 +0100

    storage username and add person fonction

--- Contact Application/starter_files/assets/js/script.js
+++ Contact Application/starter_files/assets/js/script.js
@@ -1,3 +1,21 @@
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+
+/***************/
 const rmCheck = document.getElementById("rememberMe"),
     emailInput = document.getElementById("email");
 
@@ -18,3 +36,21 @@ function lsRememberMe() {
     localStorage.checkbox = "";
   }
 }
+
+
+/***************/
+
+$("#add_person").submit(function(e){
+  e.preventDefault();
+  let formData = $(this).serialize();
+  $.post({
+    url:"/add_person.php",
+    data: formData,
+    success:function(res){
+      console.log(res);
+    },
+    error:function(err){
+      console.error(err);
+    }
+  })
+})
